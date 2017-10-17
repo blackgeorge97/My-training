88f56f76e784f2f4ef23071e8145b3b9c5cd1d2b
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-17 13:21:40 +0300

    Improve the code of the second project euler solution

--- algorithms/project-euler/problem2/2.py
+++ algorithms/project-euler/problem2/2.py
@@ -1,9 +1,9 @@
-summ=0
-a,b=1,2
-while b<4000000:
-    if b%2==0:
-        summ=summ+b
-    a,b=b,a+b
-print(summ)
+sum = 0
+a, b = 1, 2
+while b < 4000000:
+    if b%2 == 0:
+        sum = sum + b
+    a, b = b, a+b
+print(sum)
 
 
