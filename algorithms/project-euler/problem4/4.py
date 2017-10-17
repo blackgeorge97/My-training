247ee706e10cb3f5c93ffc04b49b13a1967c92a1
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-17 14:13:51 +0300

    Improve the code of the forth project euler solution

--- algorithms/project-euler/problem4/4.py
+++ algorithms/project-euler/problem4/4.py
@@ -1,10 +1,19 @@
-found=False
-for i in range(997,100,-1):
-    n=i*1000+i%10*100+i//10%10*10+i//100
-    for j in range(999,100,-1):
-        if n%j==0 and n/j>=100 and n/j<1000:
-            found=True
+for i in range(997, 100, -1):
+
+    #creates the imaged number of i
+    h = i // 100
+    d = i % 100 // 10
+    u = i % 10
+    i_im = u * 100 + d * 10 + h
+
+    #create the palindrome number
+    n = i * 1000 + i_im  
+ 
+    for j in range(999, 100, -1):
+        if n % j == 0 and n / j >= 100 and n / j < 1000:
+            print(n, j, n // j)
             break
-    if found==True:
-        break
-print(n,j,n//j)
+    else:
+        continue
+
+    break
