54732cc188b0e12756d6c178795b24def63bbec4
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-17 13:30:02 +0300

    Improve the code of the third project euler solution

--- algorithms/project-euler/problem3/3.py
+++ algorithms/project-euler/problem3/3.py
@@ -1,9 +1,9 @@
-n=int(input("Please give an integer:"))
-i=2
-while i**2<n:
-    while n%i==0:
-        n=n/i
-    i=i+1        
+n = int(input("Please give an integer:"))
+i = 2
+while i**2 < n:
+    while n%i == 0:
+        n = n / i
+    i= i + 1        
 print(n)
 
 
