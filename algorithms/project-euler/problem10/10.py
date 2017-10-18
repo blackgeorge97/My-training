1873f5d292683d26c0ea018bc93cd9e4d9f685be
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-18 20:41:06 +0300

    Add the solution of the tenth project euler problem

---
+++ algorithms/project-euler/problem10/10.py
@@ -0,0 +1,25 @@
+def is_prime(n):
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    r = int(n ** 0.5)
+    f = 5
+    while f <= r:
+        if n % f == 0:
+            return False
+        if n % (f + 2) == 0:
+            return False
+        f += 6
+    return True
+
+
+sum = 0
+for i in range(2, 2000000):
+    if is_prime(i):
+        sum += i
+print(sum)
