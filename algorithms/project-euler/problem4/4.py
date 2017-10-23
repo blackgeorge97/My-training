64babedf4720c6b2fa500981361d1b8feaf67fd3
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-23 16:06:25 +0300

    Improve the code of the fourth project euler problem

--- algorithms/project-euler/problem4/4.py
+++ algorithms/project-euler/problem4/4.py
@@ -1,19 +1,12 @@
 for i in range(997, 100, -1):
-
-    #creates the imaged number of i
-    h = i // 100
-    d = i % 100 // 10
-    u = i % 10
-    i_im = u * 100 + d * 10 + h
-
+    #create the mirrored number of i
+    i_mirrored = int(str(i)[::-1])
     #create the palindrome number
-    n = i * 1000 + i_im  
- 
+    palindrome = i * 1000 + i_mirrored  
     for j in range(999, 100, -1):
-        if n % j == 0 and n / j >= 100 and n / j < 1000:
-            print(n, j, n // j)
+        if palindrome % j == 0 and palindrome / j >= 100 and palindrome / j < 1000:
+            print(palindrome, j, palindrome // j)
             break
     else:
         continue
-
     break
