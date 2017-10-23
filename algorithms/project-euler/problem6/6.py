c1b827e99db9ff83e6798176a2d5f4407c937c23
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-23 16:32:30 +0300

    Improve the code of the sixth project euler problem

--- algorithms/project-euler/problem6/6.py
+++ algorithms/project-euler/problem6/6.py
@@ -1,8 +1,3 @@
-sum = 0
-sum_sqrs = 0
-for i in range(1, 101):
-    sum += i
-    sum_sqrs += i ** 2
-sqr_sum = sum ** 2
-print(sqr_sum - sum_sqrs)
-
+sum_of_squares = sum(i ** 2 for i in range(1, 101))
+square_of_sum = sum(range(1, 101)) ** 2
+print(square_of_sum - sum_of_squares)
