ef49e17959d00eaa7659495e8b27ecbd7462fee4
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-24 20:04:54 +0300

    Add the solution of the 14th project euler problem

---
+++ algorithms/project-euler/problem14/14.py
@@ -0,0 +1,20 @@
+LIMIT = 1000000
+max = 0
+length_cand = []
+for candidate in range(2, LIMIT):
+    count = 0
+    n = candidate
+    while n != 1:
+        count += 1
+        if n % 2 ==0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        if n < candidate and n > 2:
+            count += length_cand[int(n) - 2]
+            break
+    length_cand.append(count)
+    if max < count:
+        max = count
+        number_max = candidate
+print(number_max)
