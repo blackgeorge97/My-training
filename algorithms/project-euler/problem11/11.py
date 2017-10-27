e79858eda756286818dc64f4dd095030622f76e1
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-10-27 19:57:07 +0300

    Improve the code of the eleventh project euler problem

--- algorithms/project-euler/problem11/11.py
+++ algorithms/project-euler/problem11/11.py
@@ -1,10 +1,12 @@
+LENGTH = 20
+
 with open('p11_20x20.txt', 'r') as f:
     contents = f.read()
 lines = contents.split('\n')
 numbers = [line.split(' ') for line in lines]
 max = 0
-for i in range(0, 20):
-    for j in range(0,20):
+for i in range(0, LENGTH):
+    for j in range(0, LENGTH):
         product = 1
         if i + 3 < 20:
             for counter in range(i, i + 4):
@@ -12,19 +14,19 @@ for i in range(0, 20):
             if product > max:
                 max = product
         product = 1
-        if j + 3 < 20:
+        if j + 3 < LENGTH:
             for counter in range(j, j + 4):
                 product *= int(numbers[i][counter])
             if product > max:
                 max = product
         product = 1
-        if i + 3 < 20 and j + 3 < 20:
+        if i + 3 < LENGTH and j + 3 < LENGTH:
             for counter in range(i, i + 4):
                 product *= int(numbers[counter][counter])
             if product > max:
                 max = product
         product = 1
-        if i + 3 < 20 and j - 3 >= 0:
+        if i + 3 < LENGTH and j - 3 >= 0:
             counter2 = j
             for counter in range(i, i + 4):
                 product *= int(numbers[counter][counter2])
