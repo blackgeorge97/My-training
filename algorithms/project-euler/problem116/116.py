f618cd433b14bb16b40bfa024a2a9b3a08a25408
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-11-01 14:34:53 +0200

    Improve the code of the project euler problem 116

--- algorithms/project-euler/problem116/116.py
+++ algorithms/project-euler/problem116/116.py
@@ -7,4 +7,4 @@ for i in range(5, LENGTH + 1):
     red.append(red[i - 1] + red[i - 2] + 1)
     green.append(green[i - 1] + green[i - 3] +1)
     blue.append(blue[i - 1] + blue[i - 4] + 1)
-print(red[50] + green[50] + blue[50])
+print(red[LENGTH] + green[LENGTH] + blue[LENGTH])
