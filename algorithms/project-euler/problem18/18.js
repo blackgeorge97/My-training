7df375220db492045be9325792dd8568dc1822af
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2017-12-14 15:05:29 +0200

    Improve the coding style of the project Euler problem 18

--- algorithms/project-euler/problem18/18.js
+++ algorithms/project-euler/problem18/18.js
@@ -3,17 +3,17 @@ var contents = fs.readFileSync('pyramid.txt', 'utf8');
 
 var lines = contents.split('\n');
 var pyramid = []
-for (var i = 0; i < lines.length; i++){
+for (var i = 0; i < lines.length; i++) { 
     numbers = lines[i].split(' ');
-    for (j = 0; j < numbers.length; j++){
+    for (j = 0; j < numbers.length; j++) {
         numbers[j] = parseInt(numbers[j])
-        }
+    }
     pyramid.push(numbers);
-    }
+}
     
-for (var i = pyramid.length - 1; i; i--){
-    for (var j = 0; j < pyramid[i].length - 1; j++){
+for (var i = pyramid.length - 1; i; i--) {
+    for (var j = 0; j < pyramid[i].length - 1; j++) {
         pyramid[i - 1][j] += Math.max(pyramid[i][j], pyramid[i][j + 1])
-        }
     }
+}
 console.log(pyramid[0][0])
