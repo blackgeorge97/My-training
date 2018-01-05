ff8a61adeb99f267dfaa7ad2a443872cd726b2d4
Konstantinos Maurogeorghs <blackgeorge1997@gmail.com>
2018-01-05 18:38:35 +0200

    Add a ball shooter simulator

---
+++ ball-shooter/model.js
@@ -0,0 +1,71 @@
+function integrate() {
+    const dt = 1
+    const percVelLose = 10
+    const g = 0.001
+    const friction = 0.00001
+    
+    if (typeof ball === 'undefined') {
+        return
+    }
+    if (!ball.moving) {
+        return
+    }
+    if (!ball.changeY && ball.velocity.y + ball.radius <= 1) {
+        ball.velocity.y += g * dt
+    }
+    else if (ball.velocity.y >= 0) {
+        ball.changeY = true
+        ball.position.y = 1 - ball.radius
+        ball.velocity.y = 0
+    }
+    if (ball.velocity.y + ball.radius <= 1) {
+        ball.velocity.y += g * dt
+        if (ball.velocity.x > 0) {
+            ball.velocity.x -= friction * dt
+        }  
+        else if (ball.velocity.x < 0){
+            ball.velocity.x += friction * dt
+        }
+    }
+    ball.position.x += ball.velocity.x * dt
+    ball.position.y += ball.velocity.y * dt  
+    if (ball.position.x + ball.radius > 1
+     || ball.position.x - ball.radius < 0) {
+        if (!ball.changeX) {
+            ball.changeX = true
+            ball.velocity.x = -ball.velocity.x * (100 - percVelLose) / 100
+        } 
+    }
+    else {
+        ball.changeX = false
+    }
+    if (ball.position.y + ball.radius > 1
+     || ball.position.y - ball.radius < 0) {
+        if (!ball.changeY) {
+            ball.changeY = true
+            ball.velocity.y = -ball.velocity.y * (100 - percVelLose) / 100
+        }
+    }
+    else {
+        ball.changeY = false 
+    }  
+}
+
+class Ball {
+    constructor() {
+        this.moving = false 
+        this.changeX = false
+        this.changeY = false
+        this.stopY = false
+        this.position = new Vector(0.5, 0.5)
+        this.velocity = new Vector(0, 0)
+        this.radius = 0.02
+    }
+}
+
+class VelocityLine {
+    constructor() {
+        this.start = new Vector(0, 0)
+        this.end = new Vector(0, 0)
+    }
+}
